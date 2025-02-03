d9b8e954e8e8b3c91c1db04700a667e326891f3a
Prajakta Gite <giteprajakta61@gmail.com>
2025-02-03 10:27:29 -0800

    add initial changes

--- schema.js
+++ schema.js
@@ -20,6 +20,6 @@ module.exports.reviewSchema=Joi.object(
             rating:Joi.number().required().min(1).max(5),
             comment:Joi.string().required()
         }).required()
-
+           
     }
 );
